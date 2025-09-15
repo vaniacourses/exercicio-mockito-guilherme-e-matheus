599eee1ed65969a7c9c88793af466342022dbf69
gmoura107 <54088749+gmoura107@users.noreply.github.com>
2025-09-15 19:44:25 -0300

    Update JogoTest.java

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,31 @@
 package jogo;
 
-public class JogoTest {
-	
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+public class JogoTest {
+
+    @Test
+    public void testarJogo() {
+
+        Dado dado1 = mock(Dado.class);
+        Dado dado2 = mock(Dado.class);
+        Jogador jogador = mock(Jogador.class);
+
+
+        when(dado1.numero()).thenReturn(2);
+        when(dado2.numero()).thenReturn(3);
+        when(jogador.lancar(dado1, dado2)).thenReturn(5); 
+
+       
+        Jogo jogo = new Jogo(jogador, dado1, dado2);
+
+       
+        boolean resultado = jogo.jogo();
+
+        
+        assertEquals(true, resultado);
+    }
 }
